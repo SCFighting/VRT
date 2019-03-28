32287175e89591f6e721a4d2f9af15418a379867
SC <sc@SCdeMacBook-Pro.local>
2019-03-28 10:40:29 +0800

    添加积累

---
+++ RVT/Base/Controller/BaseController.h
@@ -0,0 +1,17 @@
+//
+//  BaseController.h
+//  RVT
+//
+//  Created by SC on 2019/3/28.
+//  Copyright © 2019 SC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaseController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RVT/Base/View/BaseView.m
@@ -0,0 +1,21 @@
+//
+//  BaseView.m
+//  RVT
+//
+//  Created by SC on 2019/3/28.
+//  Copyright © 2019 SC. All rights reserved.
+//
+
+#import "BaseView.h"
+
+@implementation BaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
