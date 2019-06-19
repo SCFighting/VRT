0c5db9791301d70343ae114290461c9d59b52f5b
SC_Fighting <hyswcvip@163.com>
2019-06-19 11:25:44 +0800

    启动图

--- RVT/Home/Controller/HomeController.m
+++ RVT/Home/Controller/HomeController.m
@@ -9,40 +9,8 @@
 #import "HomeController.h"
 
 @interface HomeController ()
-@property (nonatomic  ,  copy ) NSString *str ;
 @end
 
 @implementation HomeController
-
--(instancetype)init
-{
-    if (self = [super init]) {
-        _str = NSStringFromClass([self class]);
-    }
-    return self;
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
--(void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    NSLog(@"%@str指针地址=%p,str=%@",NSStringFromClass([self class]),&_str,_str);
-}
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    [super touchesBegan:touches withEvent:event];
-    TestController *vc = [[TestController alloc] init];
-   
-    vc.str = NSStringFromClass([self class]);
-    NSString *tt;
-    [vc method2:&tt];
-    NSLog(@"tt=%@",tt);
-    [self.navigationController pushViewController:vc animated:YES];
-}
 
 @end
